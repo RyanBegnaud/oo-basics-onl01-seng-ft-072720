99dd72a69aa3684c127b30a30c2b319638219fa8
Ryan Begnaud <ryanbegnaud13@gmail.com>
2020-08-03 19:30:17 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -14,6 +14,14 @@ class Book
   def author 
     @author 
   end
+  
+  def page_count=(page_num)
+    @page_count = page_num
+  end
+  
+  def page_count
+    @page_count
+  end
 end
 
 Book.new("And Then There Were None")
